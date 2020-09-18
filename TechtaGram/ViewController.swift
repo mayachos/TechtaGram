d6693f25ed339c929bbc18735287e04317121c82
maya <amacrk@i.softbank.jp>
2020-09-18 20:00:57 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by maya on 2020/09/18.
+//  Copyright © 2020 maya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
